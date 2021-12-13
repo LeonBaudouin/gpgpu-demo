0c7849fb7af331f9f5321716b97110bc944a9cea
Anthony LOPEZ <anthony.lopezpro@gmail.com>
2021-12-13 21:34:19 +0100

    Start project

--- src/js/webgl/Particules/index.js
+++ src/js/webgl/Particules/index.js
@@ -1,12 +1,40 @@
 import AbstractObject from '../abstract/AbstractObject'
-import modelUrl from '../../../models/500.glb'
 import * as THREE from 'three'
 
 export default class Particules extends AbstractObject {
-  constructor(context) {
+  constructor(context, { scene }) {
     super(context)
-    console.log(modelUrl)
+    this.clock = this.context.clock
+    this.renderer = this.context.renderer
+    this.amount = 32 * 32
+
     this.output = new THREE.Group()
+    this.output.add(scene)
+
+    // Créer des particules à partir d'un géom préexistante
+    this.material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.1 })
+
+    // Remplacer la géométrie par une custom
+    // this.geometry = new THREE.IcosahedronBufferGeometry(2, 3)
+    this.geometry = new THREE.BufferGeometry()
+    const array = new Float32Array(this.amount * 3)
+    
+    for (let index = 0; index < this.amount; index++) {
+      array[index * 3 + 0] = Math.random() - 0.5
+      array[index * 3 + 1] = Math.random() - 0.5
+      array[index * 3 + 2] = Math.random() - 0.5
+    }
+    this.positions = new THREE.BufferAttribute(array, 3)
+    this.geometry.setAttribute('position', this.positions)
+
+
+    // Setup le gpgpu -> Rendre une texture à l'écran
+
+    // Utiliser cette texture pour placer les particules
+
+    // Jouer avec le shader du GPGPU pour faire des trucs sympas qui bouge
+    this.points = new THREE.Points(this.geometry, this.material)
+    this.output.add(this.points)
   }
 
   tick(time, deltaTime) {}
--- src/js/webgl/Scenes/MainScene/index.ts
+++ src/js/webgl/Scenes/MainScene/index.ts
@@ -3,6 +3,9 @@ import { WebGLAppContext } from '../..'
 import AbstractObject from '../../abstract/AbstractObject'
 import AbstractObjectWithSize from '../../abstract/AbstractObjectWithSize'
 import Particules from '../../Particules'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import modelUrl from '../../../../models/500.glb'
 
 export type MainSceneContext = WebGLAppContext & {
   scene: THREE.Scene
@@ -13,6 +16,8 @@ export default class MainScene extends AbstractObjectWithSize {
   public camera: THREE.PerspectiveCamera
 
   private tickingObjects: AbstractObject[] = []
+
+  private controls: OrbitControls
 
   constructor(context: WebGLAppContext) {
     super(context)
@@ -30,6 +35,8 @@ export default class MainScene extends AbstractObjectWithSize {
   protected onResize(width: number, height: number) {
     this.camera.aspect = width / height
     this.camera.updateProjectionMatrix()
+    this.controls = new OrbitControls(this.camera, this.context.renderer.domElement)
+    this.controls.enableDamping = true
   }
 
   private setCamera() {
@@ -46,13 +53,21 @@ export default class MainScene extends AbstractObjectWithSize {
   private setObjects() {
     this.scene = new THREE.Scene()
     this.scene.background = new THREE.Color(0x1f1f1f)
-    const particles = new Particules(this.genContext())
-    this.scene.add(particles.output)
-    this.tickingObjects.push(particles)
+
+    new GLTFLoader().load(
+      modelUrl,
+      (gltf) => {
+        const particles = new Particules(this.genContext(), gltf)
+        this.scene.add(particles.output)
+        this.tickingObjects.push(particles)
+      }
+    )
+
     // this.scene.background = new THREE.Color(0xfff00)
   }
 
   public tick(...params: Parameters<AbstractObject['tick']>) {
+    this.controls.update()
     for (const obj of this.tickingObjects) {
       obj.tick(...params)
     }
